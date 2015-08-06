c5bd8655af8650c79fc7d86dac4a20c7b2ed580e
Ryan Yu <ryan.yu@tubemogul.com>
2015-08-06 16:06:18 -0700

    update

---
+++ pmWebsite/js/mockdata.js
@@ -0,0 +1,9 @@
+var mockData = {};
+
+mockData.parkingLots = [
+        {"parkingLotId": 1, "parkingLotName": "Main"},
+        {"parkingLotId": 2, "parkingLotName": "Spur"},
+        {"parkingLotId": 3, "parkingLotName": "Winterfell"},
+        {"parkingLotId": 4, "parkingLotName": "Gate"}];
+
+mockData.availableSpaces = {"availableSpaces": 10};
